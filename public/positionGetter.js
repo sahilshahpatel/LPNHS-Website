ef4075bdad2920f85111e618ff94d5b4310852bf
Benwagrez <benwagrez@gmail.com>
2017-07-22 12:45:12 -0500

    Update positionGetter.js

--- public/positionGetter.js
+++ public/positionGetter.js
@@ -10,7 +10,18 @@ document.addEventListener("DOMContentLoaded", function(){
         loadData();
     });
 });
-
+// Parse the URL parameter
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+// Give the parameter a variable name
+var eventid = getParameterByName('eventid');
 function loadData(){
     var temporaries = document.getElementsByClassName("temp");
     while(temporaries[0]){
@@ -19,108 +30,41 @@ function loadData(){
     
     var today = new Date();
     
-    firebase.database().ref("/Events").once("value").then(function(snapshot){
-        snapshot.forEach(function(childSnapshot){
             //Load My Events
-            if(document.getElementById("chapterEventsTab").classList.contains("inactive")){
-                //Is this user in the event?
                 var done = false;
-                firebase.database().ref("/Events/" + childSnapshot.key + "/Shifts").once("value").then(function(snapshot){
-                    snapshot.forEach(function(shiftsSnapshot){
-                        shiftsSnapshot.forEach(function(positionsSnapshot){
-                            positionsSnapshot.forEach(function(idSnapshot){
-                                if(!done && idSnapshot.val() === userId){
-                                    var eventName = childSnapshot.val().name;
-                                    var description = childSnapshot.val().description;
-                                    var startDate = childSnapshot.val().startDate;
-                                    var endDate = childSnapshot.val().endDate;
-                                    var location = childSnapshot.val().location;
-                                    
-                                    //format data into HTML elements
-                                    var row = document.createElement("tr");
-                                    var nameTD = document.createElement("td");
-                                        nameTD.innerHTML = eventName;
-                                        nameTD.title = description;
-                                        nameTD.classList.add("temp");
-                                    var dateTD = document.createElement("td");
-                                        if(startDate === endDate){
-                                            dateTD.innerHTML = startDate;
-                                        }
-                                        else{
-                                            dateTD.innerHTML = startDate + " to " + endDate;
-                                        }
-                                        dateTD.classList.add("temp");
-                                    var locationTD = document.createElement("td");
-                                        locationTD.classList.add("temp");
-                                    var locationLink = document.createElement("a");
-                                        locationLink.href = "https://www.google.com/maps/place/" + location; 
-                                        locationLink.innerHTML = location;
-                                    locationTD.appendChild(locationLink);
-
-                                    row.appendChild(nameTD);
-                                    row.appendChild(dateTD);
-                                    row.appendChild(locationTD);
-
-                                    //Check time - upcoming or past?
-                                    if(today>getDate(endDate)){
-                                        document.getElementById("eventHistoryTable").appendChild(row);
-                                    }
-                                    else{
-                                        document.getElementById("upcomingEventsTable").appendChild(row);
-                                    }
-                                    
-                                    done = true;
-                                }
-                            });
-                        });
-                    });
-                });
-            }
-            //Load Chapter Events
-            else{            
-                //retrieve data
-                var eventName = childSnapshot.val().name;
-                var description = childSnapshot.val().description;
-                var startDate = childSnapshot.val().startDate;
-                var endDate = childSnapshot.val().endDate;
-                var location = childSnapshot.val().location;
-                
-                //format data into HTML elements
+                var eventName = firebase.database().ref("Events/" + eventid + "/name");
+                var description = firebase.database().ref("Events/" + eventid + "/description");
+                var startDate = firebase.database().ref("Events/" + eventid + "/startDate");
+                var endDate = firebase.database().ref("Events/" + eventid + "/endDate");
+                var location = firebase.database().ref("Events/" + eventid + "/location");
+                                      //format data into HTML elements
                 var row = document.createElement("tr");
                 var nameTD = document.createElement("td");
-                    nameTD.innerHTML = eventName;
-                    nameTD.title = description;
-                    nameTD.classList.add("temp");
+                nameTD.innerHTML = eventName;
+                nameTD.title = description;
+                nameTD.classList.add("temp");
                 var dateTD = document.createElement("td");
-                    if(startDate === endDate){
-                        dateTD.innerHTML = startDate;
-                    }
-                    else{
-                        dateTD.innerHTML = startDate + " to " + endDate;
-                    }
-                    dateTD.classList.add("temp");
-                var locationTD = document.createElement("td");
-                    locationTD.classList.add("temp");
-                var locationLink = document.createElement("a");
-                    locationLink.href = "https://www.google.com/maps/place/" + location; 
-                    locationLink.innerHTML = location;
-                locationTD.appendChild(locationLink);
-
-                row.appendChild(nameTD);
-                row.appendChild(dateTD);
-                row.appendChild(locationTD);
-                
-                //Check time - upcoming or past?
-                if(today>getDate(endDate)){
-                    document.getElementById("eventHistoryTable").appendChild(row);
+                if(startDate === endDate){
+                dateTD.innerHTML = startDate;
                 }
                 else{
-                    document.getElementById("upcomingEventsTable").appendChild(row);
+                dateTD.innerHTML = startDate + " to " + endDate;
                 }
-            }
-        });
-    });
-}
+                dateTD.classList.add("temp");
+                var locationTD = document.createElement("td");
+                locationTD.classList.add("temp");
+                var locationLink = document.createElement("a");
+                                        locationLink.href = "https://www.google.com/maps/place/" + location; 
+                 locationLink.innerHTML = location;
+                 locationTD.appendChild(locationLink);
+
+                 row.appendChild(nameTD);
+                 row.appendChild(dateTD);
+                 row.appendChild(locationTD);
+
+                 done = true;
+ }
+                
 
 function getDate(stringDate){
     var month = stringDate.substring(0, stringDate.indexOf('/'));
