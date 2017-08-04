8e47355c011608036d72940a7d23ad8d85d720f7
Benwagrez <benwagrez@gmail.com>
2017-08-04 12:42:17 -0500

    Update positionGetter.js

--- public/positionGetter.js
+++ public/positionGetter.js
@@ -27,44 +27,97 @@ function loadData(){
     while(temporaries[0]){
         temporaries[0].parentElement.removeChild(temporaries[0]);
     }
-    
+
     var today = new Date();
     
-            //Load My Events
+                //Load My Events
                 var done = false;
                 var eventName = firebase.database().ref("Events/" + eventid + "/name");
                 var description = firebase.database().ref("Events/" + eventid + "/description");
                 var startDate = firebase.database().ref("Events/" + eventid + "/startDate");
                 var endDate = firebase.database().ref("Events/" + eventid + "/endDate");
                 var location = firebase.database().ref("Events/" + eventid + "/location");
-                //format data into HTML elements
-                var row = document.createElement("tr");
-                var nameTD = document.createElement("td");
-                nameTD.innerHTML = eventName;
-                nameTD.title = description;
-                nameTD.classList.add("temp");
-                var dateTD = document.createElement("td");
-                if(startDate === endDate){
-                dateTD.innerHTML = startDate;
-                }
-                else{
-                dateTD.innerHTML = startDate + " to " + endDate;
-                }
-                dateTD.classList.add("temp");
-                var locationTD = document.createElement("td");
-                locationTD.classList.add("temp");
-                var locationLink = document.createElement("a");
-                                        locationLink.href = "https://www.google.com/maps/place/" + location; 
-                 locationLink.innerHTML = location;
-                 locationTD.appendChild(locationLink);
-
-                 row.appendChild(nameTD);
-                 row.appendChild(dateTD);
-                 row.appendChild(locationTD);
-
-                 done = true;
- }
                 
+               var ref = firebase.database().ref("Events/" + eventid + "/Shifts");
+                    ref.once("value")
+                    .then(function(snapshot) {
+                    var positionId = snapshot.child("Shifts").val();
+                    });
+    
+                var positionDate = firebase.database().ref("Events/" + eventid + "/Shifts/" + positionId + "/date");
+                var positionAvailable = firebase.database().ref("Events/" + eventid + "/Shifts/" + positionId + "/positionsAvailable");
+                var positionstartTime = firebase.database().ref("Events/" + eventid + "/Shifts/" + positionId + "/startTime");
+                var positionendTime = firebase.database().ref("Events/" + eventid + "/Shifts/" + positionId + "/endTime");
+    
+                var signupB = document.createElement("BUTTON");
+                var T = document.createTextNode("SIGN UP");
+                signupB.appendChild(T);
+    
+
+                //format data into HTML elements
+                var namepE = document.getElementById("pE");
+                    namepE.innerHTML = eventName;
+                    namepE.classList.add("temp");
+                var descriptpD = document.getElementById("pD");
+                    descriptpD.innerHTML = description;
+                    descriptpD.classList.add("temp");
+                var datepT = document.getElementById("pT");
+                    if(startDate === endDate){
+                    datepT.innerHTML = startDate;
+                    }
+                    else{
+                    datepT.innerHTML = startDate + " to " + endDate;
+                    }
+                    datepT.classList.add("temp");
+                var locationpL = document.getElementById("pL");
+                    locationpL.classList.add("temp");
+                    var locationLink = document.createElement("a");
+                    locationLink.href = "https://www.google.com/maps/place/" + location; 
+                    locationLink.innerHTML = location;
+                    locationpL.appendChild(locationLink);
+                var positions = document.getElementById("PositionsTable");
+                var listing = table.insertRow(0);
+                var dateTD = listing.insertcell(0);
+                var timeTD = lising.insertcell(1);
+                var signupTD = listing.insertcell(2);
+                dateTD.innerHTML = positionDate;
+                dateTD.classList.add("temp");
+                if(positionstartTime === positionendTime){
+                    timeTD.innerHTML = positionstartTime;
+                    }
+                    else{
+                    timeTD.innerHTML = positionstartTime + " to " + positionendTime;
+                    }
+                    timeTD.classList.add("temp");
+
+                signupTD.appendChild(signupB);
+                signupTD.classList.add("temp");
+                //Singup management shindigaroo
+                var useridP = firebase.database().ref("Events/" + eventid + "/Shifts/" + positionId + "/Positions");
+                    useridP.on("value", function(snapshot) {
+                                snapshot.forEach(function(PostionSnapshot)){
+                                     if(PositionSnapshot.val() === userId && !(PositionSnapshot.val() === null)){
+                                    //if in the position
+                                        document.getElementById("signupB").value=("PENDING...");
+                                    }
+                                    else if(!(PositionSnapshot.val() === userId) && !(PositionSnapshot.val() === null)){ 
+                                        document.getElementById("signupB").addEventListener("click", signupEL); 
+                                            function signupEL(){
+                                                document.getElementById("signupB").value=("PENDING...");
+                                                //insertinfotransmittancetodatabaseandoradminsIguess
+                                                }
+                                    }
+                                    else{
+                                        document.getElementById("signupB").value=("UNAVAILABLE");
+                                    }            
+                                }
+                           })
+                
+                done = true;
+}
+                                                                        
+                                                                    
+                                                                    
 
 function getDate(stringDate){
     var month = stringDate.substring(0, stringDate.indexOf('/'));
