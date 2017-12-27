b5cd4356685853b1d792dfe22f2694500853dac2
Shadowfax246 <sahilshahpatel@gmail.com>
2017-12-27 15:39:39 -0600

    Fixed document unready error for header img

--- headerJQuery.js
+++ headerJQuery.js
@@ -1,32 +1,33 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+//<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+$(document).ready(function(){
+    //Sticky Nav (initial - prevents jump)
+    $("#navBarWrapper").css("height", $("#navBar").height());
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > $("#banner").height()) {
+            $('#navBar').addClass('navBar-fixed');
+        }
+        else{
+            $('#navBar').removeClass('navBar-fixed');
+        }
+    });
 
-//Sticky Nav (initial - prevents jump)
-$("#navBarWrapper").css("height", $("#navBar").height());
-$(window).scroll(function(){
-    if ($(window).scrollTop() > $("#banner").height()) {
-        $('#navBar').addClass('navBar-fixed');
-    }
-    else{
-        $('#navBar').removeClass('navBar-fixed');
-    }
-});
-
-//Fade-out for #LPLogo img (initial and on resize)
-var dist = $("#LPNHS").position().left - $("#LPLogo").position().left;
-if(dist>0){
-    $("#LPLogo").css("opacity", (dist-180)/70);    
-}
-$(window).resize(function(){
+    //Fade-out for #LPLogo img (initial and on resize)
     var dist = $("#LPNHS").position().left - $("#LPLogo").position().left;
     if(dist>0){
         $("#LPLogo").css("opacity", (dist-180)/70);    
     }
-});
+    $(window).resize(function(){
+        var dist = $("#LPNHS").position().left - $("#LPLogo").position().left;
+        if(dist>0){
+            $("#LPLogo").css("opacity", (dist-180)/70);    
+        }
+    });
 
-//Log In dropdown (initial-hidden)
-// $("#login").slideUp(1);
-// $("#loginSlider").click(function(){
-//     $("#login").css("visibility", "visible")
-//     .stop()
-//     .slideToggle(200);
-//});
+    //Log In dropdown (initial-hidden)
+    // $("#login").slideUp(1);
+    // $("#loginSlider").click(function(){
+    //     $("#login").css("visibility", "visible")
+    //     .stop()
+    //     .slideToggle(200);
+    //});
+});
