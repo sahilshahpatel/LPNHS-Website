a87d20a36fad318718700940d0224acad79051ec
Shadowfax246 <sahilshahpatel@gmail.com>
2017-12-27 13:34:09 -0600

    Added if statement to check active tab in events

--- events.php
+++ events.php
@@ -58,14 +58,10 @@
            $("#chapterEventsTab").click(function(){
                $(this).removeClass("inactive");
                $("#myEventsTab").addClass("inactive");
-
-               loadData();
            });
            $("#myEventsTab").click(function(){
                $(this).removeClass("inactive");
                $("#chapterEventsTab").addClass("inactive");
-
-               loadData();
            });
         });
     </script>
@@ -96,32 +92,35 @@
                     <!--Load data-->
                     <?php
                         $dom = new DOMDocument();
-                        $dom->loadHTML("events.php");
-                        
+                        $dom->loadHTML('events.php'));
+                        $dom->validate();    
+                    
                         $myEventsTab = $dom->getElementById('myEventsTab');
-                        //if($myEventsTab.hasAttribute  
-                    
-                        $sql = "SELECT * FROM events";
-                        $stmt = $pdo->prepare($sql);
-                        $stmt->execute();
-                        
-                        $eventCount = $stmt->rowCount();
-                        $eventIDs = array();
-                        array_push($eventIDs, $stmt->fetchAll(PDO::FETCH_COLUMN, 0));
-                    
-                        for($i = 0; $i<$eventCount; $i++){
-                            $sql = "SELECT * FROM events WHERE EventID=:eventID";
+                        if ($myEventsTab->hasAttribute('class') && strstr($myEventsTab->getAttribute('class'), 'inactive')):
+                            $sql = "SELECT * FROM events";
                             $stmt = $pdo->prepare($sql);
-                            $stmt->execute(["eventID" => $eventIDs[0][$i]]);
-                            $data = array();
-                            $data = $stmt->fetchAll();
-                            
-                            echo '<tr>';
-                            echo '<td title =', $data[0][2] ,'>', $data[0][1], '</td>';
-                            echo '<td>', $data[0][3], ' to ', $data[0][4], '</td>';
-                            echo '<td>', $data[0][5], '</td>';
-                            echo '</tr>';
-                        }
+                            $stmt->execute();
+
+                            $eventCount = $stmt->rowCount();
+                            $eventIDs = array();
+                            array_push($eventIDs, $stmt->fetchAll(PDO::FETCH_COLUMN, 0));
+
+                            for($i = 0; $i<$eventCount; $i++){
+                                $sql = "SELECT * FROM events WHERE EventID=:eventID";
+                                $stmt = $pdo->prepare($sql);
+                                $stmt->execute(["eventID" => $eventIDs[0][$i]]);
+                                $data = array();
+                                $data = $stmt->fetchAll();
+
+                                echo '<tr>';
+                                echo '<td title =', $data[0][2] ,'>', $data[0][1], '</td>';
+                                echo '<td>', $data[0][3], ' to ', $data[0][4], '</td>';
+                                echo '<td>', $data[0][5], '</td>';
+                                echo '</tr>';
+                            } 
+                        else:
+                            echo "hey";
+                        endif;
                     ?>
                 </table>
             </div>
