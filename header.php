389b9bce1bfdb6dfff422e0bda38cc192b7c52b3
Shadowfax246 <sahilshahpatel@gmail.com>
2018-01-02 11:37:17 -0600

    Added login/logout button to header. Logout not yet functional

--- header.php
+++ header.php
@@ -3,12 +3,19 @@
         
         <h1 class = "baseText" style = "padding-bottom: 0px; margin-bottom:0px; color: #005da3;  font-size:48px;"><span id = "LPNHS" onclick = "location.href='index.php'" title = "NHS Test - Home">Lake Park National Honor Society</span></h1>
         <h2 class = "baseText" style = "padding-top: 0px; margin-top:0px; color: #bbb; font-size:18px;">Scholarship | Leadership | Character | Service</h2>
-<<<<<<< HEAD
-		<div id = "login"><button class = "classicColor">Sign In</button></div>
-        
-=======
-                
->>>>>>> ac149d0196eab79c6ff23306ba16859cd1b46102
+		<?php if(isset($_Session["StudentID"])) : ?>
+			<div id = "headerLogout" class = "headerSignIn"><button id = "headerLogoutButton">Sign Out</button></div>
+		<?php else: ?>
+			<div id = "headerLogin" class = "headerSignIn"><button id = "headerLoginButton">Sign In</button></div>
+			<script>
+				$(document).ready(function(){
+					$("#headerLoginButton").click(function(){
+						window.location = "login.php";
+					});
+				});
+			</script>
+		<?php endif; ?>
+
     </div>
     
     <div id = "navBarWrapper">
@@ -17,9 +24,9 @@
             <a class = "baseText" id = "eventsLink" href = "events.php">Volunteer Events</a>
             <a class = "baseText" id = "members" href = "index.php">Members</a>
             <?php if(isset($_Session["StudentID"])) : ?>
-            <a class = "baseText" id = "whatItTakes" href = "index.php">What it Takes</a>
+				<a class = "baseText" id = "userProfile" href = "index.php">User Profile</a>
             <?php else :?>
-            <a class = "baseText" id = "userProfile" href = "index.php">User Profile</a>
+				<a class = "baseText" id = "whatItTakes" href = "index.php">What It Takes</a>
             <?php endif; ?>
         </nav>
     </div>
