fe5d5a37d8da9930930c078fffcd0df7a2453222
Shadowfax246 <sahilshahpatel@gmail.com>
2018-03-14 19:01:20 -0500

    changes for testing

--- requestShift.php
+++ requestShift.php
@@ -10,14 +10,15 @@
 		$shiftsList = array();
 		$shiftsList = $stmt->fetchAll();
 
+		echo 'here';
 	// Looping for every shift
 
 		for($l = 0; $l<count($shiftsList); $l++){
 
 			// Checks if that shift is picked -> $l -> shift number
-
+				echo 'l = ', $l;
 				if(isset($_POST['submit'][$l])){
-
+					echo 'found submit';
 					// Pulling data from "studentshiftrequest" to check for a repeat
 
 						$sql = "SELECT * FROM studentshiftrequests WHERE EventID = :eventID AND StudentID = :studentID AND ShiftID = :shiftID";
@@ -27,7 +28,7 @@
 					// Check for repeat
 
 						if($stmt->rowCount()===0){
-
+							echo 'no repeat';
 							// Inserting data into "studentshiftrequests"
 
 								$sql = "INSERT INTO studentshiftrequests (EventID, StudentID, ShiftID) VALUES ( :eventID, :studentID, :shiftID)";
@@ -41,5 +42,5 @@
 
 	// Rerouting user to "events.php"
 
-	header('location: events.php');
+	//header('location: events.php');
 ?>
