997ef2471402f2ebe0409466fea3c6e63037c27d
Shadowfax246 <sahilshahpatel@gmail.com>
2018-03-22 21:38:21 -0500

    [UNFINISHED] data restore

--- restoreDatabase.php
+++ restoreDatabase.php
@@ -1,8 +1,18 @@
 <?php
-    /*if(isset($_GET['filename'])){
+    if(isset($_GET['filename'])){
+        
+        $path = '.\dbBackup\nhsDataRestoreFromBackup.bat';
+        //open file to write
+        $file = fopen($path, "w");
+
+        $contents = 'cd C:\xampp\mysql\bin\n
+        mysql -u root nhs_data < C:/xampp/htdocs/lpnhs/dbBackup/'.$_GET['filename'];
+
+        fwrite($file, $contents);
+
         //double slash "\\" used to escape the \
-        $path = '.\dbBackup\\'.$_GET['filename'];
-        unlink($path);
-    }*/
+        //NOT SURE THIS WORKS YET!!!!!
+        system('cmd /c '.$path);
+    }
     header("Location: databaseBackups.php?formSubmitConfirmation=true");
 ?>
