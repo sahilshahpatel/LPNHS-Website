2d28434a5f77795011177fa190246aa3510a7b51
Shadowfax246 <sahilshahpatel@gmail.com>
2018-03-24 12:14:26 -0500

    Try 3

--- restoreDatabase.php
+++ restoreDatabase.php
@@ -5,13 +5,13 @@
         //open file to write ('.' specifies path is relative, \\ escapes the slash)
         $file = fopen('.\\'.$path, "w");
 
-        $contents = 'cd C:\xampp\mysql\bin\n
-        mysql -u root nhs_data < C:/xampp/htdocs/lpnhs/dbBackup/'.$_GET['filename'];
+        $contents = 'cd C:\xampp\mysql\bin'."\n".
+        'mysql -u root nhs_data < C:/xampp/htdocs/lpnhs/dbBackup/'.$_GET['filename'];
 
         fwrite($file, $contents);
 
         shell_exec('cd dbBackup');
-        shell_exec('/c nhsDataRestoreFromBackup.bat');
+        shell_exec('nhsDataRestoreFromBackup.bat');
     }
     header("Location: databaseBackups.php?formSubmitConfirmation=true");
 ?>
