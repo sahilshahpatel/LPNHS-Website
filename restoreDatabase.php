b16ea1304beadb63926d633fb0e5fec97d277805
Shadowfax246 <sahilshahpatel@gmail.com>
2018-03-24 11:57:51 -0500

    RestoreDatabase changes for testing

--- restoreDatabase.php
+++ restoreDatabase.php
@@ -1,17 +1,15 @@
 <?php
     if(isset($_GET['filename'])){
         
-        $path = '.\dbBackup\nhsDataRestoreFromBackup.bat';
-        //open file to write
-        $file = fopen($path, "w");
+        $path = '\dbBackup\nhsDataRestoreFromBackup.bat';
+        //open file to write ('.' specifies path is relative)
+        $file = fopen('.'.$path, "w");
 
         $contents = 'cd C:\xampp\mysql\bin\n
         mysql -u root nhs_data < C:/xampp/htdocs/lpnhs/dbBackup/'.$_GET['filename'];
 
         fwrite($file, $contents);
 
-        //double slash "\\" used to escape the \
-        //NOT SURE THIS WORKS YET!!!!!
         system('cmd /c '.$path);
     }
     header("Location: databaseBackups.php?formSubmitConfirmation=true");
