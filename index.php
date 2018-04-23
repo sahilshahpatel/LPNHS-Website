baa64f1f603d29c48d54b8bde3eed0b5e3dc40f4
Benwagrez <benwagrez@gmail.com>
2018-04-22 20:24:38 -0500

    Removing Meta

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
 ?>
 <html>
     <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>LPNHS - Home</title>
         
         <link rel="stylesheet" type="text/css" media="(max-width: 520px)" href="phoneBasedCSS.css">
