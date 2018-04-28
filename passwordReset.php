b23231dc783700a349922652b8a1531ad84df9e7
Shadowfax246 <sahilshahpatel@gmail.com>
2018-04-27 22:06:21 -0500

    Last test for today

--- passwordReset.php
+++ passwordReset.php
@@ -6,24 +6,20 @@
     $success = false; //Initially false until the database is ubdated!
 
     if(isset($_POST["submit"])){
-        $sql = "SELECT * FROM passrecovertokens WHERE Token = :token";
+        $sql = "SELECT * FROM passrecovertokens WHERE Token = :token AND Expiration >= Now()";
         $stmt = $pdo->prepare($sql);
         $stmt->execute(['token' => $_GET['token']]);
-        $tokenData = $stmt->fetch(PDO::FETCH_OBJ);
-        if($tokenData->StudentID===$_GET['userID'] && $tokenData->Expiration < date('Y-m-d')){ //Checks if studentID matches, and if token hasn't expired
+        $tokenData = array();
+        $tokenData = $stmt->fetchAll();
+        if(!empty($tokenData) && $tokenData[0][1]===$_GET['userID']){
             $sql = "UPDATE students SET PasswordHash=:passHash WHERE StudentID=:studentID";
             $stmt = $pdo->prepare($sql);
-            $success = $stmt->execute(['passHash' => password_hash($_POST['password'], PASSWORD_DEFAULT), 'studentID' => $tokenData->StudentID]);
+            $success = $stmt->execute(['passHash' => password_hash($_POST['password'], PASSWORD_DEFAULT), 'studentID' => $tokenData[0][1]]);
             if($success){ //Delete token if it has been used
                 $sql = "DELETE FROM passrecovertokens WHERE Token = :token";
                 $stmt = $pdo->prepare($sql);
                 $stmt->execute(['token' => $_GET['token']]);
             }
-        }
-        else if($tokenData->Expiration >= date('Y-m-d')){ //Delete token if it is too old
-            $sql = "DELETE FROM passrecovertokens WHERE Token = :token";
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute(['token' => $_GET['token']]);
         }
     }
 ?>
@@ -35,7 +31,6 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
         <link rel="stylesheet" href="baseCSS.css">
 
-
     </head>
 
     <header id = "header"><?php include "header.php"; ?></header>
@@ -43,7 +38,7 @@
     <body>
 		<div id = "footerPusher">
 
-            <form id="login" class="form" action="passwordReset.php?token=<?php echo $_GET['token'];?>&userID=<?php echo $_GET['userID'];?>" method="post">
+            <form class="form" action="passwordReset.php?token=<?php echo $_GET['token'];?>&userID=<?php echo $_GET['userID'];?>" method="post">
                 <div>
                     <h id="logTitle">Password Reset</h>
                     <hr class="loghr">
@@ -58,7 +53,7 @@
                     ?>
                     <input class="input2" placeholder = "Password*" type = "password" name = "password" autofocus required>
                     <br/><br/>
-                    <button id = "loginButton" type = "submit" name = 'submit' value="changePassword" style = "min-height: 75px;">Change Password</button>
+                    <button type = "submit" name = 'submit' value="changePassword" style = "min-height: 75px;">Change Password</button>
                 </div>              
             </form>
             
